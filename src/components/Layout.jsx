e0b37e7662865bf73854235b85a7da96c0d99dff
Kyle Long <kyle.long2048@gmail.com>
2024-06-13 00:05:23 +1000

    added video BG animated fire with Static img bg repeating on X to make it flow better on large screens

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,18 +4,40 @@ import Footer from './Footer'
 const Layout = ({ children }) => {
     return (
         <div className="relative h-full ">
-            <div className="h-1/2 flex justify-center items-center">
-                <div
+            <div className="h-1/2 flex  justify-center items-center -z-10 bg-repeat-x"
+                style={{
+                    backgroundImage: `url('/fire_bg.png')`,
+                    backgroundSize: 'auto 100%', // Maintain image height while allowing width to repeat
+                    backgroundPosition: 'top',
+                }}
+            >
+                <video
+                    className=""
+                    autoPlay
+                    preload="auto"
+                    loop
+                    muted
+                >
+                    <source
+                        type="video/webm"
+                        src="/animated_fire.webm"
+                    />
+                </video>
+
+                {/* STATIC IMG */}
+                {/* <div
                     className="h-full w-full min-h-[75vh] bg-repeat-x"
                     style={{
                         backgroundImage: `url('/fire_bg.png')`,
                         backgroundSize: 'auto 100%', // Maintain image height while allowing width to repeat
                         backgroundPosition: 'top',
                     }}
-                ></div>
+                ></div> */}
             </div>
             {/* This makes the img fade to black Note the absolute positions it relative to parent*/}
-            <div className="absolute inset-x-0 top-0 h-full bg-gradient-to-b from-transparent via-transparent to-black">
+            <div
+                className="absolute inset-x-0 top-0 h-full bg-gradient-to-b from-transparent via-transparent to-black"
+            >
             </div>
 
             <div className="absolute inset-x-0 top-0 h-1/2">
@@ -25,7 +47,7 @@ const Layout = ({ children }) => {
                     {/* <Footer /> */}
                 </div>
             </div>
-            
+
         </div>
 
     )
